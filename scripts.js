ca5f1b83187f17c39be63f2d4fa1008ba3ac3b3f
JamesW <JamesW@hollywoodbets.com>
2025-02-03 16:44:50 +0300

    src code

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const nav = document.querySelector('nav ul');
+    const menuIcon = document.querySelector('.menu-icon');
+    nav.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+    // ssh-keygen -t ed25519 -C "jamesnjoroge87@gmail.com"
+
+  }
+
+ 
